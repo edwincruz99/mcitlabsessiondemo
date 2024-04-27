d4743c8cfc527fb4cf2394b2931cd43ba7b9aec5
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-04-26 20:25:49 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,12 +1,22 @@
 locals{
 	servicename = "abracadabra"
 	forum = "abracadabramcit"
+        firstname = "edwin"
+        lastname = "cruz"
+        city = "lasalle"
 }
- 
 output "print"{
 value = local.servicename
 }
- 
-output "secondprint" {
+ output "secondprint" {
 	value = local.forum
+}
+output "printfirstname"{
+value = local.firstname
+}
+output "printlastname"{
+value = local.lastname
+}
+output "printcity"{
+value = local.city
 }
