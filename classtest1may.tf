34a14a456f1f0a40885359eb40c280cacee4acca
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-05-01 19:20:29 -0400

    Update classtest1may.tf

--- classtest1may.tf
+++ classtest1may.tf
@@ -1,6 +1,18 @@
-variable "original_string" {
+variable "original_string2" {
   default = "edwin alexander cruz"
 }
-output "string_length" {
+output "string_length2" {
   value = length(var.original_string)
+}
+ 
+output "uppercase_string2" {
+  value = upper(var.original_string2)
+}
+
+output "lowercase_string2" {
+  value = lower(var.original_string2)
+}
+ 
+variable "additional_string2" {
+  default = " from Terraform"
 }
