f6dfb4329571d32c759e4eb2122321f02ad4c7b2
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-05-03 17:12:48 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -1,5 +1,10 @@
 variable "string_list3" {
   default = ["Egip", "Italy","Colombia","Argentina","New Zeland"]
 }
+
+variable "{countries to visit"
+    type=string
+    default= ["Egip", "Italy","Colombia","Argentina","New Zeland"]
+}
   
 
