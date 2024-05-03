22c3dd36e0eaea437661b904513a88c01068f922
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-05-03 17:26:38 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -7,6 +7,10 @@ variable "countries_to_visit" {
     default= "Egip+Italy+Colombia+Argentina+New Zeland"
 }
 
+output "string_length2" {
+  value = length(var.countries_to_visit)
+}
+
 output "string_list3_output"{
     value=var.string-list3
 }
