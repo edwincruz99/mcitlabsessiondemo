49e171fbebeb2cb9a6234282f473d43f8792633f
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-05-06 18:40:51 -0400

    Update examcode01.tf

--- examcode01.tf
+++ examcode01.tf
@@ -4,3 +4,9 @@ variable "exam_list" {
 output "exam_q13" {
   value = join(" ", var.exam_list)
 }
+locals {
+  contains_keyword1 = contains(split(" ", var.exam_list), "azure")
+}
+output "string_contains_no_sample1" {
+  value = local.contains_keyword1 ? "String contains 'azure'" : "String does not contain 'azure'"
+}
