a92ae2033070f0b63737ddf99959da1aefa39b24
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-05-06 19:55:44 -0400

    Update examcode01.tf

--- examcode01.tf
+++ examcode01.tf
@@ -4,16 +4,6 @@ variable "exam_list" {
 output "exam_q13" {
   value = join(" ", var.exam_list)
 }
-variable "lis_exam1" {
-  type    = list(list(string))
-  default = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
-}
-
-flatten_exam = (var.list_exam1)
-
-output "flattened_exam_result" {
-  value = local.flattened_exam
-}
 
 
 
