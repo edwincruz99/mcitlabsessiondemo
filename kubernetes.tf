b3cb391a3f1e51a58d8b9d416bdabfe77e21e5ba
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-05-06 20:08:02 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,3 +1,7 @@
 locals{ 
   cluster_names=["k10abc","k10abc1","k11abc2","k12abc3","k13abc4"]
 }
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "myfirstterraform_resource_group"
+  location = "Canada Central"
+}
